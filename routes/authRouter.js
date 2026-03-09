35ae4ac4e88c7a42606856845d8f767c00508a0f
yuriiavr <94492796+yuriiavr@users.noreply.github.com>
2026-03-09 04:13:09 +0200

    111

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -4,16 +4,25 @@ import {
   login,
   logout,
   getCurrent,
+  updateAvatar
 } from "../controllers/authControllers.js";
 import validateBody from "../helpers/validateBody.js";
 import { registerSchema, loginSchema } from "../schemas/authSchemas.js";
+import upload from "../middlewares/upload.js";
 import authenticate from "../middlewares/authenticate.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", validateBody(registerSchema), register);
 authRouter.post("/login", validateBody(loginSchema), login);
+
 authRouter.post("/logout", authenticate, logout);
 authRouter.get("/current", authenticate, getCurrent);
+authRouter.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatarURL"),
+  updateAvatar
+);
 
 export default authRouter;
